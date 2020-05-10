62c9596059cec8f40bd46ce883dabf06fdc59ca0
KavishkaRathnaweera <62067264+KavishkaRathnaweera@users.noreply.github.com>
2020-05-10 17:37:33 +0530

    Create officer.sql

---
+++ model/mysql/officer.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 10, 2020 at 02:06 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dmt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `officer`
+--
+
+CREATE TABLE `officer` (
+  `id` varchar(100) NOT NULL,
+  `passwrd` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `officer`
+--
+
+INSERT INTO `officer` (`id`, `passwrd`) VALUES
+('Counter1', 'c1password'),
+('Counter2', 'c2password'),
+('Cashier', 'capassword'),
+('Examinar', 'expassword'),
+('Licencecounter', 'lcpassword'),
+('Permitcounter', 'pcpassword'),
+('Admin', 'adpassword');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
